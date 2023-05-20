55640600decf9b44673376e1170290b17d089ee6
UrskaBenedik <134137309+UrskaBenedik@users.noreply.github.com>
2023-05-20 16:27:39 +0200

    Drugo nalaganje spletne karte dveh poti
    Datoteka prikazuje spletno karto dveh različnih poti. Ena prikazuje pot Gorskega maratona štirih občin, druga pa pot Tržiške planinske poti.

---
+++ data/Gorskimaratontirihobinwaypoints_5.js
@@ -0,0 +1,1 @@
+var json_Gorskimaratontirihobinwaypoints_5 = {"type":"FeatureCollection","name":"Gorskimaratontirihobinwaypoints_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
