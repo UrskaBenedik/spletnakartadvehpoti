5ca133b165d3e085268e2f353012df98ffb74ede
UrskaBenedik <134137309+UrskaBenedik@users.noreply.github.com>
2023-05-20 15:28:23 +0200

    Prvo nalaganje spletne karte dveh poti
    Prikaz spletne karte dveh različnih poti. Ena je iz Gorskega maratona štirih občin, druga pa iz dela Slovenske planinske poti.

---
+++ data/Sled_T242014waypoints_5.js
@@ -0,0 +1,1 @@
+var json_Sled_T242014waypoints_5 = {"type":"FeatureCollection","name":"Sled_T242014waypoints_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
